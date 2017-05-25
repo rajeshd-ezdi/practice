8d65727656d420908b7d6a0bdc2e84b8ee9de0fa
rajeshd-ezdi <rajesh.d@ezdi.us>
2017-05-25 19:54:43 +0530

    hello

---
+++ src/main/java/ezdi/pr0023/mo003/App.java
@@ -0,0 +1,13 @@
+package ezdi.pr0023.mo003;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
