b218c9746a6fd8c643a39db85e5c6fcd5e624bbd
rajeshd-ezdi <rajesh.d@ezdi.us>
2017-05-25 15:58:58 +0530

    second

--- product1/src/main/java/ezdi/test1/product1/App2.java
+++ product1/src/main/java/ezdi/test1/product1/App2.java
@@ -1,7 +1,8 @@
 package ezdi.test1.product1;
 
-public class App {
+public class App2 {
+
 	public static void main(String [] args){
-		System.out.println("this is second commit");
+		System.out.println("update");
 	}
 }
